2538371f2efa5df293b0829b3288550b20cd4f75
BuildTools <coucketimon@gmail.com>
2025-11-22 21:40:19 +0100

    fix: made command descriptions more clear

--- frontend/src/data/builtInCommands.js
+++ frontend/src/data/builtInCommands.js
@@ -3,36 +3,35 @@ export const built_in_commands = [
     name: "/ce add <enchantment> [level]",
     permission: "add",
     description:
-      "Applies the specified enchantment to the item currently held by the player, with an optional level. Set 'allow_unsafe_enchantments' to true in config.yml for unsafe enchantments (same functionality as vanilla /enchant command)",
+      "Applies the <enchantment> to the item currently held by the player, with an optional [level].",
   },
   {
     name: "/ce remove <enchantment>",
     permission: "remove",
     description:
-      "Remove a specific enchantment from the item currently held by the player.",
+      "Remove the <enchantment> from the item currently held by the player.",
   },
   {
     name: "/ce create",
     permission: "create",
     description:
-      "Quickly design and configure custom enchantments using the web builder, then seamlessly load them into your server without manual file editing.",
+      "Design and configure a new custom enchantment using the web builder and load them back into the server with the click of a button.",
   },
   {
-    name: "/ce edit",
+    name: "/ce edit <enchantment>",
     permission: "edit",
     description:
-      "Quickly update your custom enchantments using the web builder and reload them without needing to manually modify configuration files.",
+      "Edit the <enchantment> using the web builder and reload them back into the server with the click of a button.",
   },
   {
     name: "/ce info",
     permission: "info",
     description:
-      "Get an overview of your available default and custom enchantments.",
+      "Get an overview of all available default and loaded custom enchantments.",
   },
   {
     name: "/ce reload",
     permission: "reload",
-    description:
-      "Reloads the plugin configuration files.",
+    description: "Reload the plugin's configuration files.",
   },
 ];
